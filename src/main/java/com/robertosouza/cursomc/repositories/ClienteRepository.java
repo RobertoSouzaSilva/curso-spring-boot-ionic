33628f93830c4cfaa2ed4d19249b8c251ad28a38
Roberto Souza <robertosouza57@yahoo.com.br>
2019-05-31 20:52:32 -0300

    validação customizada: email repetido

--- src/main/java/com/robertosouza/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/robertosouza/cursomc/repositories/ClienteRepository.java
@@ -1,13 +1,17 @@
 package com.robertosouza.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.robertosouza.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-	
+
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 
 }
