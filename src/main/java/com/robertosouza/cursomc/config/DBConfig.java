c8bea00de7593f2ff242035ce410855fe7ddc08c
Roberto Souza <robertosouza57@yahoo.com.br>
2019-06-10 12:00:11 -0300

    Criando profile de Teste

---
+++ src/main/java/com/robertosouza/cursomc/config/DBConfig.java
@@ -0,0 +1,25 @@
+package com.robertosouza.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.robertosouza.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class DBConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instanteateDataBase() throws ParseException {
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+
+}
